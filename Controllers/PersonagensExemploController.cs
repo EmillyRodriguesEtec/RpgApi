83c8e19eb558a9c78e96e0daab7e6b372396e73d
Emilly.R-Henrique.N <Emilly.R-Henrique.N@test>
2023-02-27 09:05:31 -0300

    RpgApi Atualização

--- Controllers/PersonagensExemploController.cs
+++ Controllers/PersonagensExemploController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using RpgApi.Models;
 using RpgApi.Models.Enuns;
+using  System.Linq;
 
 namespace RpgApi.Controllers
 {
@@ -30,15 +31,72 @@ namespace RpgApi.Controllers
             new Personagem() { Id = 7, Nome = "Radagast", PontosVida=100, Forca=25, Defesa=11, Inteligencia=35, Classe=ClasseEnum.Mago }
         };
 
+        [HttpGet("GetAll")]
         public IActionResult Get()
         {
             return Ok(personagens);
         }
 
+        [HttpGet("Get")]
         public IActionResult GetFirst()
         {
             Personagem p = personagens[0];
-            return OK(p);
+            return Ok(p);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingle(int id)
+        {
+            return Ok(personagens.FirstOrDefault(pe => pe.Id == id));
+        }
+
+        [HttpPost]
+        public IActionResult AddPersonagem(Personagem novoPersonagem)
+        {
+            personagens.Add(novoPersonagem);
+            return Ok(personagens);
+        }
+
+        [HttpPut]
+        public IActionResult UpdatePersonagem(Personagem p)
+        {
+            Personagem personagemAlterado = personagens.Find(pers => pers.Id == p.Id);
+            personagemAlterado.Nome = p.Nome;
+            personagemAlterado.PontosVida = p.PontosVida;
+            personagemAlterado.Forca = p.Forca;
+            personagemAlterado.Defesa = p.Defesa;
+            personagemAlterado.Inteligencia = p.Inteligencia;
+            personagemAlterado.Classe = p.Classe;
+
+            return Ok(personagens);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            personagens.RemoveAll(pers => pers.Id == id);
+
+            return Ok(personagens);
+        }
+
+        [HttpGet("GetOrdenado")]
+        public IActionResult GetOrdem()
+        {
+            List<Personagem> listaFinal = personagens.OrderBy(p => p.Id).ToList();
+
+            return Ok(listaFinal);
+        }
+
+        [HttpGet("GetContagem")]
+        public IActionResult GetQuantidade()
+        {
+            return Ok("Quantidade de personagens: " + personagens.Count);
+        }
+
+        [HttpGet("GetSomaForca")]
+        public IActionResult GetSomaForca()
+        {
+            return Ok(personagens.Sum(p => p.Forca));
         }
     }
 }
